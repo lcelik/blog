c1041db88f6838ad401efd09a7e283f64fa1ac6e
lcelik <lidijacelik87@gmail.com>
2022-07-05 00:54:20 +0100

    commiting changes

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
-app.use(express.json());
 
-// const uri =
-//   "mongodb+srv://lidi:lidi@cluster0.p8kup.mongodb.net/sample_blog?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -33,7 +42,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
-res.status(200).json("File has been uploaded");
+  res.status(200).json("File has been uploaded");
 });
 
 app.use("/api/auth", authRoute);
@@ -41,6 +50,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
-  console.log("Back-end is running");
+app.listen(PORT, () => {
+  console.log("Your server is running on port" + " " + PORT);
 });
